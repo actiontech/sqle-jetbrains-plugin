334523d6fa92dc3b3ffc1c3e13f973ea71573148
taolx0 <taolx25@foxmail.com>
2023-10-18 18:45:16 +0800

    chore:ui

--- src/main/java/com/actiontech/sqle/from/SQLEAuditResultUI.java
+++ src/main/java/com/actiontech/sqle/from/SQLEAuditResultUI.java
@@ -1,8 +1,7 @@
 package com.actiontech.sqle.from;
 
 import com.actiontech.sqle.config.*;
-import com.actiontech.sqle.util.EllipsisRenderer;
-import com.actiontech.sqle.util.Util;
+import com.actiontech.sqle.util.*;
 import com.github.vertical_blank.sqlformatter.SqlFormatter;
 
 import javax.swing.*;
@@ -13,6 +12,7 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +39,10 @@ public class SQLEAuditResultUI {
         return rootPanel;
     }
 
+    JTable sqlAuditResultTable = new JTable();
+
+    LinkedList<AuditResult> auditResultList;
+
     JTable table = new JTable();
     DefaultTableModel model = (DefaultTableModel) table.getModel();
 
@@ -59,7 +63,7 @@ public class SQLEAuditResultUI {
             model.addRow(item);
         }
 
-        FitOverviewTableSize(table);
+        FitTableSize(table);
         JTableHeader jTableHeader = table.getTableHeader();
         tableJPanel.add(jTableHeader, BorderLayout.NORTH);
         tableJPanel.add(table, BorderLayout.CENTER);
@@ -74,9 +78,29 @@ public class SQLEAuditResultUI {
         sqlAuditResultPanel.removeAll();
         sqlAuditResultPanel.setBorder(new TitledBorder(new EtchedBorder(), "SQL审核结果"));
 
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, tableJPanel, createRightPanel(sqlDetailPanel, sqlAuditResultPanel, tableMetaDataJpanel));
+        JScrollPane jScrollPane = new JScrollPane(splitPane);
+        getRootPanel().add(jScrollPane);
+
+
         if (result.getSQLResults().size() > 0) {
             showSQLAuditDetail(result, 0);
         }
+    }
+
+    private JPanel createRightPanel(JPanel rightPanel1, JPanel rightPanel2, JPanel rightPanel3) {
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new GridLayout(2, 1));
+        topPanel.add(rightPanel1);
+        topPanel.add(rightPanel2);
+
+        rightPanel.add(topPanel, BorderLayout.NORTH);
+        rightPanel.add(rightPanel3, BorderLayout.CENTER);
+
+        return rightPanel;
     }
 
     private void showSQLAuditDetail(SQLEAuditResult result, int row) {
@@ -91,18 +115,29 @@ public class SQLEAuditResultUI {
         sqlDetailPanel.add(sqlTable, BorderLayout.CENTER);
         FitTableSize(sqlTable);
 
-        // load sql audit result
-        JTable sqlAuditResultTable = new JTable();
-        DefaultTableModel sqlAuditResultTableModel = (DefaultTableModel) sqlAuditResultTable.getModel();
-        sqlAuditResultTableModel.setColumnIdentifiers(new String[]{""});
         List<SQLEAuditResultItem> sqlResults = result.getSQLResults();
-        String auditResultHtml = generateHtml(sqlResults.get(row).getAuditResult());
-        sqlAuditResultTableModel.addRow(new String[]{auditResultHtml});
+        auditResultList = sqlResults.get(row).getAuditResult();
+
+        String[] columnNames = {"规则", "知识库"};
+
+        Object[][] data = new Object[auditResultList.size()][columnNames.length];
+        for (int i = 0; i < auditResultList.size(); i++) {
+            AuditResult auditResult = auditResultList.get(i);
+            String auditStr = String.format("[%s]%s", auditResult.getLevel(), auditResult.getMessage());
+            data[i][0] = generateHtml(auditStr);
+            data[i][1] = generateHtml("点击查看知识库");
+        }
+
+        sqlAuditResultTable = new JTable(data, columnNames);
+
+        sqlAuditResultTable.getColumnModel().getColumn(0).setCellRenderer(new StringRenderer());
+        sqlAuditResultTable.getColumnModel().getColumn(1).setCellRenderer(new ButtonRenderer());
+        sqlAuditResultTable.getColumnModel().getColumn(1).setCellEditor(new ButtonEditor());
+
         JTableHeader sqlAuditResultTableHeader = sqlAuditResultTable.getTableHeader();
         sqlAuditResultPanel.add(sqlAuditResultTableHeader, BorderLayout.NORTH);
         sqlAuditResultPanel.add(sqlAuditResultTable, BorderLayout.CENTER);
         FitTableSize(sqlAuditResultTable);
-
 
         if (analysisResult.size() > 0) {
             JTable jTable = new JTable();
@@ -135,32 +170,9 @@ public class SQLEAuditResultUI {
         }
     }
 
-    public void FitOverviewTableSize(JTable myTable) {
-        FitOverviewWidthSize(myTable);
-        FitTableHeight(myTable);
-    }
-
     public void FitTableSize(JTable myTable) {
         FitTableWidth(myTable);
         FitTableHeight(myTable);
-    }
-
-    public void FitOverviewWidthSize(JTable table) {
-        final TableColumnModel columnModel = table.getColumnModel();
-        for (int column = 0; column < table.getColumnCount(); column++) {
-            TableColumn tableColumn = columnModel.getColumn(column); // 替换columnIndex为您要设置的列索引
-            if (column == 0) {
-                tableColumn.setMinWidth(50); // 设置最小宽度
-                tableColumn.setMaxWidth(50); // 设置最大宽度
-            } else if (column == 1) {
-                tableColumn.setCellRenderer(new EllipsisRenderer());
-                tableColumn.setMaxWidth(500);
-                tableColumn.setMinWidth(500);
-            } else if (column == 2) {
-                tableColumn.setMinWidth(60); // 设置最小宽度
-                tableColumn.setMaxWidth(60); // 设置最大宽度
-            }
-        }
     }
 
     public void FitTableHeight(JTable myTable) {
@@ -174,32 +186,26 @@ public class SQLEAuditResultUI {
         }
     }
 
-    public void FitTableWidth(JTable table) {
-        final TableColumnModel columnModel = table.getColumnModel();
-        for (int column = 0; column < table.getColumnCount(); column++) {
-            TableColumn tableColumn = columnModel.getColumn(column);
-            tableColumn.setMinWidth(1000);
-            tableColumn.setMaxWidth(1000);
+    public void FitTableWidth(JTable myTable) {
+        final TableColumnModel columnModel = myTable.getColumnModel();
+        for (int column = 0; column < myTable.getColumnCount(); column++) {
+            int width = myTable.getTableHeader().getColumnModel().getColumn(column).getWidth();
+            for (int row = 0; row < myTable.getRowCount(); row++) {
+                TableCellRenderer renderer = myTable.getCellRenderer(row, column);
+                Component comp = myTable.prepareRenderer(renderer, row, column);
+                width = Math.max(comp.getPreferredSize().width, width);
+            }
+
+            DefaultTableCellRenderer r = new DefaultTableCellRenderer();
+            if (column == 0) {
+                r.setHorizontalAlignment(JLabel.LEFT);
+                columnModel.getColumn(column).setCellRenderer(r);
+            } else {
+                width += myTable.getFont().getSize() + myTable.getIntercellSpacing().getWidth();
+            }
+
+            columnModel.getColumn(column).setPreferredWidth(width);
         }
-//        final TableColumnModel columnModel = myTable.getColumnModel();
-//        for (int column = 0; column < myTable.getColumnCount(); column++) {
-//            int width = myTable.getTableHeader().getColumnModel().getColumn(column).getWidth();
-//            for (int row = 0; row < myTable.getRowCount(); row++) {
-//                TableCellRenderer renderer = myTable.getCellRenderer(row, column);
-//                Component comp = myTable.prepareRenderer(renderer, row, column);
-//                width = Math.max(comp.getPreferredSize().width, width);
-//            }
-//
-//            DefaultTableCellRenderer r = new DefaultTableCellRenderer();
-//            if (column == 0) {
-//                r.setHorizontalAlignment(JLabel.CENTER);
-//                columnModel.getColumn(column).setCellRenderer(r);
-//            } else {
-//                width += myTable.getFont().getSize() + myTable.getIntercellSpacing().getWidth();
-//            }
-//
-//            columnModel.getColumn(column).setPreferredWidth(width);
-//        }
     }
 
     public String generateHtml(String text) {
@@ -243,5 +249,66 @@ public class SQLEAuditResultUI {
                 }
             }
         });
+    }
+
+    static class ButtonRenderer extends JButton implements TableCellRenderer {
+        public ButtonRenderer() {
+            setOpaque(false);  // 设置按钮为透明
+            setBorderPainted(false);  // 设置不绘制按钮的边框
+            setFocusPainted(false);  // 设置不绘制按钮的焦点样式
+            setContentAreaFilled(false);
+        }
+
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            setText((value != null) ? value.toString() : "");
+            return this;
+        }
+    }
+
+    class ButtonEditor extends AbstractCellEditor implements TableCellEditor {
+        private JButton button;
+
+        public ButtonEditor() {
+            sqlAuditResultTable.addMouseMotionListener(new MouseAdapter() {
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    button = new JButton("点击查看知识库");
+                    int rowPoint = sqlAuditResultTable.rowAtPoint(e.getPoint());
+                    button.addActionListener(e1 -> {
+                        String ruleName = auditResultList.get(rowPoint).getRuleName();
+
+                        SQLESettings settings = SQLESettings.getInstance();
+                        HttpClientUtil client = new HttpClientUtil(settings);
+                        String knowledge;
+                        try {
+                            knowledge = client.GetRuleKnowledge(settings.getDBType(), ruleName);
+                        } catch (Exception ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                        NewWindow newWindow = new NewWindow();
+                        JLabel label = new JLabel(generateHtml(knowledge));
+
+                        label.setVerticalAlignment(JLabel.TOP);
+                        label.setHorizontalAlignment(JLabel.LEFT);
+
+                        JScrollPane scrollPane = new JScrollPane(label);
+
+                        newWindow.setContentPane(scrollPane);
+                        newWindow.setVisible(true);
+
+                        fireEditingStopped();
+                    });
+                }
+            });
+        }
+
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+            return button;
+        }
+
+        public Object getCellEditorValue() {
+            return button.getText();
+        }
     }
 }
