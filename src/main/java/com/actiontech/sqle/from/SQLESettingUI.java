3918bf15cce4d422c92ee8a55b18789032d99bf8
littleniannian <jarviscrick@outlook.com>
2025-04-02 16:44:59 +0800

    feat: 增加使用token登陆的方式-前端

--- src/main/java/com/actiontech/sqle/from/SQLESettingUI.java
+++ src/main/java/com/actiontech/sqle/from/SQLESettingUI.java
@@ -1,31 +1,49 @@
 package com.actiontech.sqle.from;
 
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.ItemEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Observable;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+
 import com.actiontech.sqle.config.InstStatusBarWidget;
 import com.actiontech.sqle.config.SQLESettings;
 import com.actiontech.sqle.config.SchemaStatusBarWidget;
 import com.actiontech.sqle.util.HttpClientUtil;
 import com.actiontech.sqle.util.NotifyUtil;
+import com.google.common.collect.Lists;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.ui.DocumentAdapter;
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Observable;
-
+import static com.actiontech.sqle.constant.Constant.LOGIN_TYPE_PASSWORD;
+import static com.actiontech.sqle.constant.Constant.LOGIN_TYPE_TOKEN;
 
 public class SQLESettingUI extends Observable {
     private JPanel rootPanel;
     private JTextField sqleAddr;
+    private JComboBox<String> loginTypeBox;
     private JTextField sqleUserName;
     private JPasswordField sqlePassword;
+    private JTextField token;
+    private JLabel userNameLabel;
+    private JLabel passwordLabel;
+    private JLabel tokenLabel;
     private JButton testConnBtn;
     private JRadioButton httpBtn;
     private JRadioButton httpsBtn;
@@ -36,6 +54,8 @@ public class SQLESettingUI extends Observable {
     public static SQLESettings settings;
 
     public static HashMap<String, String> projects;
+
+    public static List<String> loginTypes = Lists.newArrayList(LOGIN_TYPE_PASSWORD, LOGIN_TYPE_TOKEN);
 
     public SQLESettingUI(SQLESettings settings) {
         this.settings = settings;
@@ -50,11 +70,16 @@ public class SQLESettingUI extends Observable {
     private void loadSettings(SQLESettings settings) {
         String addr = settings.getSQLEAddr();
         sqleAddr.setText(addr);
+
         String username = settings.getUserName();
         sqleUserName.setText(username);
         String pwd = settings.getPassword();
         sqlePassword.setText(pwd);
-
+        String tokenValue = settings.getAccessToken();
+        token.setText(tokenValue);
+        loginTypeBox.removeAllItems();
+        String loginType = settings.getLoginType();
+        loginTypeBox.addItem(loginType);
         String projectName = settings.getProjectName();
         projectBox.removeAllItems();
         projectBox.addItem(projectName);
@@ -78,8 +103,10 @@ public class SQLESettingUI extends Observable {
 
     public void apply(SQLESettings settings) {
         settings.setSQLEAddr(sqleAddr.getText());
+        settings.setLoginType(loginTypeBox.getSelectedItem().toString());
         settings.setUserName(sqleUserName.getText());
         settings.setPassword(new String(sqlePassword.getPassword()));
+        settings.setAccessToken(token.getText());
         settings.setDBType((String) dbTypeBox.getSelectedItem());
         settings.setProjectName((String) projectBox.getSelectedItem());
         settings.setDataSourceName((String) dbDataSourceBox.getSelectedItem());
@@ -92,7 +119,16 @@ public class SQLESettingUI extends Observable {
         String dataSourceName = (String) dbDataSourceBox.getSelectedItem();
         String schemaName = (String) SchemaBox.getSelectedItem();
 
-        return !sqleAddr.getText().equals(ObjectUtils.defaultIfNull(settings.getSQLEAddr(), "")) || (httpsBtn.isSelected() != settings.isEnableHttps()) || !sqleUserName.getText().equals(ObjectUtils.defaultIfNull(settings.getUserName(), "")) || !(new String(sqlePassword.getPassword())).equals(ObjectUtils.defaultIfNull(settings.getPassword(), "")) || !ObjectUtils.defaultIfNull(dbType, "").equals(settings.getDBType()) || !ObjectUtils.defaultIfNull(projectName, "").equals(settings.getProjectName()) || !ObjectUtils.defaultIfNull(dataSourceName, "").equals(settings.getDataSourceName()) || !ObjectUtils.defaultIfNull(schemaName, "").equals(settings.getSchemaName());
+        return !sqleAddr.getText().equals(ObjectUtils.defaultIfNull(settings.getSQLEAddr(),
+                "")) || (httpsBtn.isSelected() != settings.isEnableHttps()) || !sqleUserName.getText()
+                .equals(ObjectUtils.defaultIfNull(settings.getUserName(), "")) || !(new String(
+                sqlePassword.getPassword())).equals(
+                ObjectUtils.defaultIfNull(settings.getPassword(), "")) || !ObjectUtils.defaultIfNull(dbType, "")
+                .equals(settings.getDBType()) || !ObjectUtils.defaultIfNull(projectName, "")
+                .equals(settings.getProjectName()) || !ObjectUtils.defaultIfNull(dataSourceName, "")
+                .equals(settings.getDataSourceName()) || !ObjectUtils.defaultIfNull(schemaName, "")
+                .equals(settings.getSchemaName()) || !loginTypeBox.getSelectedItem().toString().equals(settings.getLoginType())
+                || !(token.getText()).equals(ObjectUtils.defaultIfNull(settings.getAccessToken(), ""));
     }
 
     private void loadListener() {
@@ -110,6 +146,63 @@ public class SQLESettingUI extends Observable {
                 settings.setSQLEAddr(sqleAddr.getText());
             }
         });
+
+        loginTypeBox.addItemListener(itemEvent -> {
+            if (itemEvent.getStateChange() == ItemEvent.SELECTED) {
+                Object loginTypeValueObject = loginTypeBox.getSelectedItem();
+                if (loginTypeValueObject != null) {
+                    settings.setLoginType(loginTypeValueObject.toString());
+                }
+
+                if (StringUtils.equals(settings.getLoginType(), LOGIN_TYPE_PASSWORD)) {
+                    settings.setLoginType(LOGIN_TYPE_PASSWORD);
+                    tokenLabel.setVisible(false);
+                    userNameLabel.setVisible(true);
+                    passwordLabel.setVisible(true);
+                    token.setVisible(false);
+                    sqlePassword.setVisible(true);
+                    sqleUserName.setVisible(true);
+
+                } else if (StringUtils.equals(settings.getLoginType(), LOGIN_TYPE_TOKEN)) {
+                    settings.setLoginType(LOGIN_TYPE_TOKEN);
+                    tokenLabel.setVisible(true);
+                    userNameLabel.setVisible(false);
+                    passwordLabel.setVisible(false);
+                    sqlePassword.setVisible(false);
+                    sqleUserName.setVisible(false);
+                    token.setVisible(true);
+                }
+            }
+        });
+
+        loginTypeBox.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                Object selected = loginTypeBox.getSelectedItem();
+                String selectedItem;
+                if (selected != null) {
+                    selectedItem = selected.toString();
+                } else {
+                    selectedItem = "";
+                }
+                loginTypeBox.removeAllItems();
+                loginTypes.forEach(loginType -> {
+                    loginTypeBox.addItem(loginType);
+                    if (loginType.equals(selectedItem)) {
+                        loginTypeBox.setSelectedItem(loginType);
+                        settings.setLoginType(loginType);
+                    }
+                });
+                loginTypeBox.updateUI();
+                loginTypeBox.setPopupVisible(true);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+
         sqleUserName.getDocument().addDocumentListener(new DocumentAdapter() {
             @Override
             protected void textChanged(@NotNull DocumentEvent e) {
@@ -121,6 +214,13 @@ public class SQLESettingUI extends Observable {
             protected void textChanged(@NotNull DocumentEvent e) {
                 String passwordContent = new String(sqlePassword.getPassword());
                 settings.setPassword(passwordContent);
+            }
+        });
+
+        token.getDocument().addDocumentListener(new DocumentAdapter() {
+            @Override
+            protected void textChanged(@NotNull DocumentEvent e) {
+                settings.setAccessToken(token.getText());
             }
         });
 
@@ -136,8 +236,11 @@ public class SQLESettingUI extends Observable {
         testConnBtn.addActionListener(event -> {
             try {
                 HttpClientUtil client = new HttpClientUtil(settings);
-                String token = client.Login();
-                settings.setToken(token);
+                if (StringUtils.equals(settings.getLoginType(), LOGIN_TYPE_PASSWORD)) {
+                    client.Login();
+                } else {
+                    projects = client.GetProjectList();
+                }
                 NotifyUtil.showTipsDialog("Test Connection", "Test Connection Success");
                 projectBox.setFocusable(true);
             } catch (Exception e) {
@@ -149,10 +252,6 @@ public class SQLESettingUI extends Observable {
         projectBox.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
-                if (sqleAddr.getText().equals("") || sqleUserName.getText().equals("") || new String(sqlePassword.getPassword()).equals("")) {
-                    return;
-                }
-
                 Object selected = projectBox.getSelectedItem();
                 String selectedItem;
                 if (selected != null) {
